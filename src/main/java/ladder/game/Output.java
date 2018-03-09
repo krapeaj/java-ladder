851547a5221de78a1d72f8b32d8c06aab6d98931
Jae Park <jaeepark@gmail.com>
2018-03-09 21:16:53 +0900

    initial

--- src/main/java/ladder/game/Output.java
+++ src/main/java/ladder/game/Output.java
@@ -4,4 +4,20 @@ public class Output {
     public static void print(String message) {
         System.out.println(message);
     }
+
+    public static String returnLadderFrame() {
+        return "|";
+    }
+
+    public static String returnLadderStep() {
+        return "-";
+    }
+
+    public static String returnEmptyString() {
+        return " ";
+    }
+
+    public static String returnNewLine() {
+        return "\n";
+    }
 }
