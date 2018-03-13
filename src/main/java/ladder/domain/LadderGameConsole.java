d976341201e5ff848b27f92dd4cdcff7b55860a3
krapeaj <jaeepark@gmail.com>
2018-03-13 23:20:09 +0900

    Replace while loops with try/catch statements and recursion, and move methods concerned to Input class

--- src/main/java/ladder/domain/LadderGameConsole.java
+++ src/main/java/ladder/domain/LadderGameConsole.java
@@ -3,37 +3,17 @@ package ladder.domain;
 import ladder.view.Input;
 import ladder.view.Output;
 
+import java.util.InputMismatchException;
+
 public class LadderGameConsole {
-    private static final int MIN_HEIGHT = 2;
 
     public static void main(String[] args) {
         startGame();
     }
 
-    private static String promptUserForNames() {
-        Output.print("참여할 사람 이름을 쉼표(,)를 기준으로 입력하세요.");
-        String nameString = Input.takeString();
-
-        while (Input.isNotValidString(nameString)) {
-            Output.print("다섯 글자 이하인 이름을 최소 두 개 입력해주세요.");
-            nameString = Input.takeString();
-        }
-        return nameString;
-    }
-
-    private static int promptUserForLadderHeight() {
-        Output.print("최대 사다리 높이는 몇 개 인가요?");
-        int userInput = Input.takeInt();
-        while (userInput <= MIN_HEIGHT) {
-            Output.print("2 보다 큰 숫자를 입력해주세요.");
-            userInput = Input.takeInt();
-        }
-        return userInput;
-    }
-
     private static void startGame() {
-        String playerNames = promptUserForNames();
-        int ladderHeight = promptUserForLadderHeight();
+        String playerNames = Input.promptUserForNames();
+        int ladderHeight = Input.promptUserForLadderHeight();
         LadderGame ladderGame = new LadderGame(playerNames, ladderHeight);
 
         String ladderString = ladderGame.buildLadder();
--- src/main/java/ladder/view/Input.java
+++ src/main/java/ladder/view/Input.java
@@ -6,26 +6,31 @@ import java.util.Scanner;
 public class Input {
     private static final int CHAR_LIMIT = 5;
     private static final int MIN_NUMBER_OF_NAMES = 2;
+    private static final int MIN_HEIGHT = 2;
 
-    public static int takeInt() {
-        Scanner scan = new Scanner(System.in);
-        try {
-            return scan.nextInt();
-        }
-        catch (InputMismatchException e){
-            return 0;
-        }
+    public static String promptUserForNames() {
+        System.out.println("참여할 사람 이름을 쉼표(,)를 기준으로 입력하세요:");
+        String names = Input.takeString();
+
+        return checkNamesValidity(names);
     }
 
-    public static String takeString() {
+    private static String takeString() {
         Scanner scan = new Scanner(System.in);
         return scan.next();
     }
 
-    public static boolean isNotValidString(String nameString) {
-        String[] names = nameString.split(",");
-
-        return isNotEnoughNames(names) || isOverCharLimit(names);
+    private static String checkNamesValidity(String names){
+        String[] splitNames = names.split(",");
+        if(isNotEnoughNames(splitNames)){
+            System.out.print("최소 두명의 이름을 입력해주세요. ");
+            return promptUserForNames();
+        }
+        if(isOverCharLimit(splitNames)){
+            System.out.print("이름은 다섯 자 이하만 입력 가능합니다. ");
+            return promptUserForNames();
+        }
+        return names;
     }
 
     private static boolean isOverCharLimit(String[] names){
@@ -38,7 +43,30 @@ public class Input {
     }
 
     private static boolean isNotEnoughNames(String[] names){
-
         return names.length < MIN_NUMBER_OF_NAMES;
+    }
+
+    public static int promptUserForLadderHeight() {
+        Output.print("최대 사다리 높이는 몇 개 인가요?");
+        return Input.checkHeightValidity(Input.takeInt());
+    }
+
+    private static int takeInt() {
+        Scanner scan = new Scanner(System.in);
+        try {
+            return scan.nextInt();
+        }
+        catch (InputMismatchException e){
+            System.out.println("숫자를 입력해주세요.");
+            return takeInt();
+        }
+    }
+
+    private static int checkHeightValidity(int userInput) {
+        if(userInput <= MIN_HEIGHT){
+            System.out.println("2보다 큰 숫자를 입력해주세요.");
+            return promptUserForLadderHeight();
+        }
+        return userInput;
     }
 }
